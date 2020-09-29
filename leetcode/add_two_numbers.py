f060ee41038580ef2a53b5dc2a52aca13623d513
Gunhoon Lee <gunhoon@gmail.com>
2020-09-29 10:37:04 +0900

    Improve Problems #2

--- leetcode/add_two_numbers.py
+++ leetcode/add_two_numbers.py
@@ -12,6 +12,40 @@ class Solution:
         Output: 7 -> 0 -> 8
         Explanation: 342 + 465 = 807.
         """
+        head = None
+        last = None
+
+        carry = 0
+        while l1 is not None or l2 is not None or carry != 0:
+            value = carry
+            if l1 is not None:
+                value += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                value += l2.val
+                l2 = l2.next
+
+            carry = value // 10
+
+            curr = ListNode(value % 10, None)
+
+            if head is None:
+                head = curr
+
+            if last is None:
+                last = curr
+            else:
+                last.next = curr
+                last = curr
+
+        return head
+
+    def addTwoNumbersOld(self, l1: ListNode, l2: ListNode) -> ListNode:
+        """
+        Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+        Output: 7 -> 0 -> 8
+        Explanation: 342 + 465 = 807.
+        """
         input1 = self.convertListToNumber(l1)
         input2 = self.convertListToNumber(l2)
 
